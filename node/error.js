3b5c740f2b10ed2774f3a0edb1e818e05e1e6f8c
houserqu <houserqu@tencent.com>
2020-05-21 19:59:22 +0800

    some file

---
+++ node/error.js
@@ -0,0 +1,8 @@
+const m = 1
+
+try {
+  const m = 2
+} catch (error) {
+}
+
+console.log('end')
---
+++ node/lodash/inintersection.js
@@ -0,0 +1,3 @@
+const _ = require('lodash')
+
+console.log(_.intersection(['1', '2', '3'], ['1', '2', '1']))
